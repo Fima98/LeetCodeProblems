f9af5f1782e9bde059b7b200ee852b9e6cc93747
Fima <loboda.fima@icloud.com>
2025-07-27 14:34:56 +0300

    Add solution for Roman to Integer problem

---
+++ 13. Roman to Integer/main.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def romanToInt(self, s):
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        total = 0
+        prev_value = 0
+
+        for char in reversed(s):
+            current = roman[char]
+            if current < prev_value:
+                total -= current
+            else:
+                total += current
+            prev_value = current
+        return total
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    s = "MCMXCIV"
+    result = solution.romanToInt(s)
+    print(result)  # Output: 1994
