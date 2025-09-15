074be1cd38b4b73007ac5bf9720abcff454c68b7
fima98 <loboda.fima@icloud.com>
2025-09-15 18:36:52 +0000

    14. Longest Common Prefix problem

---
+++ 14. Longest Common Prefix/main.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            while prefix != "":
+                if strs[i][:len(prefix)] != prefix:
+                    prefix = prefix[:-1]
+                else:
+                    break
+        return prefix
+    
+if __name__ == "__main__":
+    # example
+    s = Solution()
+    print(s.longestCommonPrefix(["flower","flow","flight"]))  # expected "fl"
+    print(s.longestCommonPrefix(["dog","racecar","car"]))     # expected ""
